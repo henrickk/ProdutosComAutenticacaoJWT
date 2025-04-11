c77de4c3b0026a3faf9e44924344f7b38bfbb744
Henrick Adrian <henrick.adrian4@gmail.com>
2025-04-10 21:44:01 -0300

    Adicionando o método Get para buscar pedido por id

--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -7,7 +7,7 @@ namespace ProdutosComAutenticacaoJWT.Controllers
 {
     [ApiController]
     [Route("api/produtos")]
-    public class ProdutosController
+    public class ProdutosController : ControllerBase
     {
         private readonly ApiDbContext _context;
         public ProdutosController(ApiDbContext context)
@@ -22,6 +22,20 @@ namespace ProdutosComAutenticacaoJWT.Controllers
         public async Task<ActionResult<IEnumerable<Produto>>> BuscarProdutos()
         {
             return await _context.Produtos.ToListAsync();
+        }
+
+        [HttpGet]
+        [Route("BuscarProdutoPorId/{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<Produto>> BuscarProdutoPorId(int id)
+        {
+            var produto = await _context.Produtos.FindAsync(id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            return produto;
         }
     }
 }
