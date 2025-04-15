7326f106e7d69d5580d99fd2d02f50dde80aa654
Henrick Adrian <henrick.adrian4@gmail.com>
2025-04-15 02:28:28 -0300

    Implementando JWT na API.

---
+++ Models/JwtSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace ProdutosComAutenticacaoJWT.Models;
+internal class JwtSettings
+{
+    public string? Segredo { get; set; }
+    public int TempoExpiracao { get; set; }
+    public string? Emissor { get; set; }
+    public string? Audiencia { get; set; }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,22 @@
+using ProdutosComAutenticacaoJWT.Data;
+using ProdutosComAutenticacaoJWT.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using ProdutosComAutenticacaoJWT.Data;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers()
+    .ConfigureApiBehaviorOptions(options =>
+    {
+        options.SuppressModelStateInvalidFilter = true;
+    });
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ApiDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -11,9 +25,29 @@ builder.Services.AddIdentity<IdentityUser, IdentityRole>()
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApiDbContext>();
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+var JwtSettingsSection = builder.Configuration.GetSection("JwtSettings");
+builder.Services.Configure<JwtSettings>(JwtSettingsSection);
+
+var jwtSettings = JwtSettingsSection.Get<JwtSettings>();
+var key = Encoding.ASCII.GetBytes(jwtSettings.Segredo);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = true;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = true,
+        ValidIssuer = jwtSettings.Emissor,
+        ValidateAudience = true,
+        ValidAudience = jwtSettings.Audiencia
+    };
+});
 
 var app = builder.Build();
 
