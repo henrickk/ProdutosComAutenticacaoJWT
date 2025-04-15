b885dc76159c45c4e3a2ed799d41fff6375497e2
Henrick Adrian <henrick.adrian4@gmail.com>
2025-04-15 02:38:04 -0300

    Controller de autenticação pt-1.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProdutosComAutenticacaoJWT.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController : ControllerBase
+    {
+        // Aqui você pode injetar o serviço de autenticação e o contexto do banco de dados, se necessário.
+        // private readonly IAuthenticationService _authService;
+        // private readonly ApiDbContext _context;
+        public AuthController()
+        {
+            // _authService = authService;
+            // _context = context;
+        }
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] RegisterUserViewModel model)
+        {
+            return Ok();
+        }
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginUserViewModel model)
+        {
+            return Ok();
+        }
+    }
+    {
+    }
+}
